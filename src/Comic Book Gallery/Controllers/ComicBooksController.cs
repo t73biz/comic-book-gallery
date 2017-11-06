ab57a459c7694f030bf9d38fa9cb68be4aed5aa8
Ronald Chaplin <rchaplin@t73.biz>
2017-11-05 18:55:56 -0600

    Added ComicBooks Detail View

--- src/Comic Book Gallery/Controllers/ComicBooksController.cs
+++ src/Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace Comic_Book_Gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello World");
+            return View();
         }
     }
 }
